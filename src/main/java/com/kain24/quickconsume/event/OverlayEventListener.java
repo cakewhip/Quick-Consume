e342e76df95ac024c01ab0613b0cb7f28a804234
phan-kavin <phan.kavin@gmail.com>
2019-01-04 01:28:42 -0500

    Change render event to Post to avoid double rendering

--- src/main/java/com/kain24/quickconsume/event/OverlayEventListener.java
+++ src/main/java/com/kain24/quickconsume/event/OverlayEventListener.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class OverlayEventListener {
     @SubscribeEvent
-    public static void renderHotbar(RenderGameOverlayEvent e) {
+    public static void renderHotbar(RenderGameOverlayEvent.Post e) {
         if(e.getType() == RenderGameOverlayEvent.ElementType.HOTBAR) {
             GuiFoodSlot.drawSlot(e.getResolution());
         }
