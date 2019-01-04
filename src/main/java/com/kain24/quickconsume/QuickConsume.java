7d1f97ee419678b5643afb6ebafb007418b21ac3
phan-kavin <phan.kavin@gmail.com>
2019-01-03 20:31:57 -0500

    Correct mod details

--- src/main/java/com/kain24/quickconsume/QuickConsume.java
+++ src/main/java/com/kain24/quickconsume/QuickConsume.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.kain24.quickconsume;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,25 +7,21 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+@Mod(modid = QuickConsume.MODID, name = QuickConsume.NAME, version = QuickConsume.VERSION)
+public class QuickConsume {
+    public static final String MODID = "quickconsume";
+    public static final String NAME = "Quick Consume";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        logger = event.getModLog();
+    public void preInit(FMLPreInitializationEvent e) {
+        logger = e.getModLog();
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
+    public void init(FMLInitializationEvent e) {
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 }
