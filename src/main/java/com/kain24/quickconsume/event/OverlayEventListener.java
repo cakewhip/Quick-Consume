20223224f8736155f8d4cf2e14e670299d9026a8
phan-kavin <phan.kavin@gmail.com>
2019-01-03 23:11:53 -0500

    Remove event listener priority (useless)

--- src/main/java/com/kain24/quickconsume/event/OverlayEventListener.java
+++ src/main/java/com/kain24/quickconsume/event/OverlayEventListener.java
@@ -2,11 +2,10 @@ package com.kain24.quickconsume.event;
 
 import com.kain24.quickconsume.client.gui.GuiFoodSlot;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class OverlayEventListener {
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    @SubscribeEvent
     public static void renderHotbar(RenderGameOverlayEvent e) {
         if (e.getType() == RenderGameOverlayEvent.ElementType.HOTBAR) {
             GuiFoodSlot.drawSlot(e.getResolution());
