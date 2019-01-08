b16849365a46ee39a9356e190a074789ce17cada
phan-kavin <phan.kavin@gmail.com>
2019-01-08 16:51:16 -0500

    Fixed versioning

--- src/main/java/com/kain24/quickconsume/QuickConsume.java
+++ src/main/java/com/kain24/quickconsume/QuickConsume.java
@@ -13,25 +13,8 @@ public class QuickConsume {
     public static final String MODID = "quickconsume";
     public static final String NAME = "Quick Consume";
 
-    /**
-     * MCVERSION
-     * Always matches the Minecraft version the mod is for.
-     * MAJORMOD
-     * Removing items, blocks, tile entities, etc.
-     * Changing or removing previously existing mechanics.
-     * Updating to a new Minecraft version.
-     * MAJORAPI
-     * Changing the order or variables of enums.
-     * Changing return types of methods.
-     * Removing public methods altogether.
-     * MINOR
-     * Adding items, blocks, tile entities, etc.
-     * Adding new mechanics.
-     * Deprecating public methods. (This is not a MAJORAPI increment since it doesn’t break an API.)
-     * PATCH
-     * Bugfixes.
-     */
-    public static final String VERSION = "1.12.2-1.0.1.0";
+
+    public static final String VERSION = "@VERSION@";
 
     public static Logger logger;
 
